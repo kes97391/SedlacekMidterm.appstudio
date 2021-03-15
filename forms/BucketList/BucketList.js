5f3c9922a0ead2025a35672e1760b1d141c57a4c
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-03-15 14:38:39 -0500

    Changes so far

---
+++ forms/BucketList/BucketList.js
@@ -0,0 +1,47 @@
+let userBucketList = ["Go to Mexico", "Get my Degree", "Buy a car"]
+
+btnView.onclick=function(){
+  let Display = lblViewItems.value
+  lblViewItems.value = userBucketList
+}
+
+btnAddItem.onclick=function(){
+  userBucketList.push(inptBucketList.value)
+  lblViewItems.value = userBucketList
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let dogsRUsItems = ["Crocheted Dog Toy $10.00", "Slow Feeder Bowl 14.00", "Pawz Grey Treat Jar $20.00", "Blue Sweet Dreams Thermal Pajamas $30.00", "Checker Chewy Vuiton Bowl $32.00", "Red Heart Sweater Dress $27.00", "Velvet Hair Bow - Sun Kissed $12.00", "Into the Wild Leash $27.00"]
+
+btnWishList.onclick=function(){
+  let Display = lblProductsAvailable.value 
+  lblProductsAvailable.value = dogsRUsItems
+}
+
+
+let wishList = []
+
+btnDesiredItem.onclick=function() {
+  let addition = inptNewItem.value
+  wishList.push(inptNewItem.value)
+  lblDesiredItem.value = wishList
+}
+*/
+
+
+
+
+btnView.onclick=function(){
+  
+}
