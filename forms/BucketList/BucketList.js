bc9d166c0c95b574740134b44d4085c4e90152b9
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-03-15 15:09:05 -0500

    Final Bucket List

--- forms/BucketList/BucketList.js
+++ forms/BucketList/BucketList.js
@@ -20,23 +20,6 @@ btnAddItem.onclick=function(){
 
 
 
-/*
-let dogsRUsItems = ["Crocheted Dog Toy $10.00", "Slow Feeder Bowl 14.00", "Pawz Grey Treat Jar $20.00", "Blue Sweet Dreams Thermal Pajamas $30.00", "Checker Chewy Vuiton Bowl $32.00", "Red Heart Sweater Dress $27.00", "Velvet Hair Bow - Sun Kissed $12.00", "Into the Wild Leash $27.00"]
-
-btnWishList.onclick=function(){
-  let Display = lblProductsAvailable.value 
-  lblProductsAvailable.value = dogsRUsItems
-}
-
-
-let wishList = []
-
-btnDesiredItem.onclick=function() {
-  let addition = inptNewItem.value
-  wishList.push(inptNewItem.value)
-  lblDesiredItem.value = wishList
-}
-*/
 
 
 
