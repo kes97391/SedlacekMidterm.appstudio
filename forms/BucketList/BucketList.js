0f1f4714031feb2c14c8fc28a01da682458f6628
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-03-15 15:17:23 -0500

    Final

---
+++ forms/BucketList/BucketList.js
@@ -0,0 +1,29 @@
+/*
+let userBucketList = ["Buy Car", "Sell House", "Go to Florida"]
+
+for(let i = 0; i < userBucketList.length; i++) {
+  console.log(userBucketList[i])
+}
+
+btnView.onclick=function(){
+  let Display = lblViewItems.value
+  lblViewItems.value = userBucketList
+}
+
+btnAddItem.onclick=function(){
+  userBucketList.push(inptBucketList.value)
+  lblViewItems.value = userBucketList
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
